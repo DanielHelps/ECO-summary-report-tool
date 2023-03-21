317f1b97aef4fd3553d997c3ce8bb8af15fb91d9
daniel <mdan1000@gmail.com>
2023-03-21 14:17:20 +0200

    Initial commit

---
+++ BOM_report.py
@@ -0,0 +1,218 @@
+
+def main(username, password, ECO, pb, value_text, root):
+    
+    from requests.auth import HTTPBasicAuth
+    import xlsxwriter
+    import requests
+    import copy
+    import os
+    from datetime import datetime
+
+    # username = 'daniel.marom@kornit.com'
+    # password = 'Kornit@2023'
+    auth = HTTPBasicAuth(username, password)
+    # ECO = 'ECO-10028-23'
+    check_assemblies = ['23','33','06']
+    params = {'q':'ChangeNotice='+ECO}
+    # pb['value'] = 10
+    # root.update()
+    response = requests.get('https://fa-evbp-saasfaprod1.fa.ocs.oraclecloud.com/fscmRestApi/resources/11.13.18.05/productChangeOrdersV2', auth=auth,params=params)
+    if response.status_code == 401:
+        return 401
+    pb['value']=0
+    response_json =  response.json()
+    affected_object_link = response_json['items'][0]['links'][2]['href']
+    response_affected = requests.get(affected_object_link,auth=auth)
+    response_affected_json = response_affected.json()
+    if os.path.isfile(f'{ECO}_Summary_report.xlsx'):
+        os.remove(f'{ECO}_Summary_report.xlsx')
+    workbook = xlsxwriter.Workbook(f'{ECO}_Summary_report.xlsx')
+    worksheet = workbook.add_worksheet()
+    worksheet.set_column(0, 0, 27.43)
+    worksheet.set_column(1, 1, 7.14)
+    worksheet.set_column(2, 2, 8.14)
+    worksheet.set_column(3, 3, 19.14)
+    worksheet.set_column(5, 5, 7.71)
+    worksheet.set_column(6, 6, 10.14)
+    worksheet.set_column(7, 7, 11.29)
+    
+
+    bold = workbook.add_format({'bold': True})
+    blue = workbook.add_format({'font_color':'blue'})
+    purple = workbook.add_format({'font_color':'purple'})
+    red = workbook.add_format({'font_color':'red'})
+    green = workbook.add_format({'font_color':'green'})
+
+    worksheet.write(0,0, f'{ECO} summary report',bold)
+    worksheet.write(1,0, f"ECO name: {response_json['items'][0]['ChangeName']}",bold)
+    worksheet.write(2,0, f"ECO description: {response_json['items'][0]['Description']}",bold)
+    
+    worksheet.write(4,0, 'Item P/N',bold)
+    worksheet.write(4,1, 'Old Rev',bold)
+    worksheet.write(4,2, 'New Rev',bold)
+    worksheet.write(4,3, 'Changed Component',bold)
+    worksheet.write(4,4, 'Old qty',bold)
+    worksheet.write(4,5, 'New qty',bold)
+    worksheet.write(4,6, 'Qty change',bold)
+    worksheet.write(4,7, 'Change type',bold)
+
+
+    def remove_a_from_b(a, b):
+        a_copy = copy.deepcopy(a)
+        b_copy = copy.deepcopy(b)
+        for i, b_item in enumerate(b_copy):
+                match_in_a = [item for item in a_copy if item[0] == b_item[0]]
+                for item in match_in_a:
+                    b_copy[i] = (b_copy[i][0], b_copy[i][1]-item[1]) 
+        return list(filter(lambda x: x[1] > 0,b_copy))
+
+    def add_comp_to_report(component, qty_change, old_qty, new_qty, change, assembly, i):
+        worksheet.write(i,0,assembly['ItemNumber'], purple)
+        worksheet.write(i,1,assembly['OldRevision'], purple)
+        worksheet.write(i,2,assembly['NewItemRevision'], purple)
+        color = green if change == "Added" else red
+        worksheet.write(i,3,component, color)
+        worksheet.write(i,4,old_qty, color)
+        worksheet.write(i,5,new_qty, color)
+        worksheet.write(i,6,qty_change, color)
+        worksheet.write(i,7,change, color)
+        
+            
+        
+        
+
+    def find_BOM_change(OLD_BOM, checked_list, added, removed, ECO_num):
+        OLD_BOM_ITEMS = requests.get(OLD_BOM,auth=auth, params={'limit':1000}).json()
+        for item in checked_list:
+            matched_old_bom = [v for i, v in enumerate(OLD_BOM_ITEMS['items']) if v['ComponentItemNumber'] == item[0] and v['ChangeNotice']==ECO_num]
+            change_in_bom = 0
+            if len(matched_old_bom)>1:
+                sorted_date_bom = sorted(matched_old_bom, key=lambda x: datetime.strptime(x['LastUpdateDateTime'][0:10], '%Y-%m-%d'))
+                old_qty = sorted_date_bom[0]['Quantity']
+                new_qty = sorted_date_bom[1]['Quantity']
+                change_in_bom = new_qty - old_qty
+            elif len(matched_old_bom)==1:
+                matched_old_bom = [v for i, v in enumerate(OLD_BOM_ITEMS['items']) if v['ComponentItemNumber'] == item[0]]
+                if len(matched_old_bom)>1:
+                    matched_old_bom = sorted(matched_old_bom, key=lambda x: datetime.strptime(x['LastUpdateDateTime'][0:10], '%Y-%m-%d'))
+                    if matched_old_bom[1]['EndDateTime'] == None:
+                        old_qty = matched_old_bom[0]['Quantity']
+                        new_qty = matched_old_bom[1]['Quantity']
+                        change_in_bom = new_qty - old_qty
+                    else:
+                        old_qty = matched_old_bom[1]['Quantity']
+                        new_qty = matched_old_bom[0]['Quantity']
+                        change_in_bom = new_qty - old_qty
+                elif len(matched_old_bom)==1:
+                    old_qty = 0
+                    new_qty = matched_old_bom[0]['Quantity']
+                    change_in_bom= new_qty
+            a = item[0],abs(change_in_bom),old_qty,new_qty
+            if change_in_bom>0:
+                added.append(a)
+            elif change_in_bom<0:
+                removed.append(a)
+            
+        # for item in OLD_BOM_ITEMS['items']:
+        #     updated_component = [v for i, v in enumerate(updated) if v[0] == item['ComponentItemNumber']]
+        #     if updated_component != []:
+        #         if item['Quantity'] < updated_component[0][1]:
+        #             removed.append(updated_component[0])
+        #             updated.remove(updated_component[0])
+        #         elif item['Quantity'] > updated_component[0][1]:
+        #             added.append(updated_component[0])
+        #             updated.remove(updated_component[0])
+
+
+            # elif updated_component != []:
+            #     if item['Quantity'] > updated_component[0]:
+            #         removed.append(tuple())
+            
+        return added, removed
+            
+    i = 5
+    num_of_items = len(response_affected_json['items'])
+    progress_value = 100/num_of_items
+    for affected_item in response_affected_json['items']:
+        
+        print(f"Item {affected_item['ItemNumber']}, changes from Rev {affected_item['OldRevision']} to Rev {affected_item['NewItemRevision']}")
+        worksheet.write(i,0,affected_item['ItemNumber'], blue)
+        worksheet.write(i,1,affected_item['OldRevision'], blue)
+        worksheet.write(i,2,affected_item['NewItemRevision'], blue)
+        i+=1
+
+        if affected_item['ItemNumber'][0:2] in check_assemblies:
+            affected_item_structure_link = affected_item['links'][6]['href']
+            response_affected_structure = requests.get(affected_item_structure_link,auth=auth).json()
+            if response_affected_structure['items'] != []:
+                response_affected_structure_comps = requests.get(response_affected_structure['items'][0]['links'][3]['href'],auth=auth,params={'limit':1000}).json()
+                OLD_BOM = f"https://fa-evbp-saasfaprod1.fa.ocs.oraclecloud.com:443/fscmRestApi/resources/11.13.18.05/itemStructures/{response_affected_structure_comps['items'][0]['BillSequenceId']}/child/Component"
+                added = []
+                removed = []
+                updated = []
+
+                for component in response_affected_structure_comps['items']:
+                    
+                    if component['ChangeId'] != None:
+                        # print(component['ComponentItemNumber'])
+                        if component['ACDTypeCode'] == 1:
+                            added.append(tuple((component['ComponentItemNumber'], component['ComponentQuantity'])))
+                        elif component['ACDTypeCode'] == 2:
+                            updated.append(tuple((component['ComponentItemNumber'], component['ComponentQuantity'])))
+                        else:
+                            removed.append(tuple((component['ComponentItemNumber'], component['ComponentQuantity'])))
+                pass
+
+                
+                
+                removed_in_end = remove_a_from_b(added,remove_a_from_b(updated, removed))
+                updated_in_end = remove_a_from_b(added,remove_a_from_b(removed,updated))
+                added_in_end = remove_a_from_b(removed, remove_a_from_b(updated,added))
+
+                if updated_in_end != []:
+                    added_in_end, removed_in_end = find_BOM_change(OLD_BOM, updated_in_end, added_in_end, removed_in_end, ECO)
+
+                if added_in_end != []:
+                    added_in_end, removed_in_end = find_BOM_change(OLD_BOM, added_in_end, [], removed_in_end, ECO)
+
+                if removed_in_end != []:
+                    added_in_end, removed_in_end = find_BOM_change(OLD_BOM, removed_in_end, added_in_end, [], ECO)
+
+
+                for item in removed_in_end:
+                    if len(item) == 4:
+                        add_comp_to_report(item[0],item[1],item[2],item[3],'Removed',affected_item,i)
+                    else:
+                        add_comp_to_report(item[0],item[1],"","",'Removed',affected_item,i)
+                    i+=1
+                
+                # for item in updated_in_end:
+                #     add_comp_to_report(item[0],item[1],'Qty changed',affected_item,i)
+                #     i+=1
+                
+                for item in added_in_end:
+                    if len(item) == 4:
+                        add_comp_to_report(item[0],item[1],item[2],item[3],'Added',affected_item,i)
+                    else:
+                        add_comp_to_report(item[0],item[1],"","",'Added',affected_item,i)
+                    i+=1
+
+                print(f'Removed: {removed_in_end}')
+                print(f'Added: {added_in_end}')
+                print(f'Updated: {updated_in_end}')
+                # for removed_item in removed:
+                #     if removed_item[1] == 0:
+                #         removed.remove(removed_item)
+        pb['value'] += progress_value
+        value_text['text'] = f"{round(pb['value'], 2)}%"
+        root.update()
+    
+        
+    workbook.close()
+    return f'{ECO}_Summary_report.xlsx'
+
+if __name__ == "__main__":
+   # stuff only to run when not called via 'import' here
+   main(None, None, None, None, None, None)
+
+
---
+++ BOM_report_GUI.py
@@ -0,0 +1,138 @@
+import tkinter as tk
+from tkinter import ttk
+import os
+import BOM_report
+import tkinter.messagebox
+import pickle
+from requests.auth import HTTPBasicAuth
+import xlsxwriter
+import os
+from tkinter import StringVar
+from functools import partial   
+import sys
+
+def resource_path(relative_path):
+    try:
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def save_nd_quit():
+    config = {
+        'username': username.get(),
+        'password': password.get(),
+    }
+
+    with open("saved_settings.dat", "wb") as pickle_file:
+        pickle.dump(config, pickle_file, pickle.HIGHEST_PROTOCOL)
+    root.quit()
+    
+
+def load():
+    with open("saved_settings.dat", "rb") as pickle_file:
+        config = pickle.load(pickle_file)
+
+    username.set(config.get('username'))
+    password.set(config.get('password'))
+
+root = tk.Tk()
+root.geometry('420x230')
+root.resizable(False, False)
+root.title('ECO summary report tool')
+title_font = ("Helvetica", 16)
+regular_font = ("Helvetica", 12)
+root.columnconfigure(0, weight=1)
+root.columnconfigure(1, weight=1)
+root.iconbitmap(resource_path("icon.ico"))
+
+def BOM_report_creation(username, password, ECO_num):
+
+    # tkinter.messagebox.showerror("ERROR!", f'Username {username.get()}, Password {password.get()}, ECO # {ECO_num.get()}')
+    
+    # BOM_report.main(username.get(),password.get(),ECO_num.get())
+
+    # username = 'daniel.marom@kornit.com'
+    # password = 'Kornit@2023'
+    # ECO_num = 'ECO-10029-23'
+   
+    if ECO_num.get() != "":
+        try:
+
+            response_code = BOM_report.main(username.get(),password.get(),ECO_num.get(), pb, value_label, root)
+        except PermissionError:
+            tkinter.messagebox.showerror("Permission error", "Permission error, please exit the open ECO report and try again")
+        except IndexError:
+            tkinter.messagebox.showerror("ECO error", "Couldn't find ECO, please check ECO #")
+        except ZeroDivisionError:
+            tkinter.messagebox.showerror("ECO error", "0 items in ECO... Cannot create ECO report")
+            
+        except Exception as e:
+            tkinter.messagebox.showerror("Unkown error", e)
+        else:
+            if response_code==401:
+                tkinter.messagebox.showerror("Authorization error", "Authorization error, please check your username and password!")
+            else:
+                tkinter.messagebox.showinfo("Report complete", f"""Report complete! 
+you can find the it in the current directory at 
+   {response_code}""")
+    else:
+        tkinter.messagebox.showerror("ECO error", "Can't have empty ECO #")
+            
+
+
+ttk.Label(
+    root,
+    text='Oracle username',
+    font=title_font).grid(row=0, column=0, sticky=tk.W, padx=10)
+
+username = StringVar()
+usernameEntry = ttk.Entry(root, textvariable=username).grid(row=0, column=1, sticky=tk.W, ipadx=40, padx=(0,15))
+
+
+ttk.Label(
+    root,
+    text='Oracle password',
+    font=title_font).grid(row=1, column=0, sticky=tk.W, padx=10)
+
+password = StringVar()
+passwordEntry = ttk.Entry(root, textvariable=password, show='*').grid(row=1, column=1, sticky=tk.W, ipadx=40, padx=(0,15))  
+
+
+if os.path.isfile(f'saved_settings.dat'):
+    load()
+
+ttk.Label(
+    root,
+    text='ECO #',
+    font=title_font).grid(row=2, column=0, sticky=tk.W, padx=12)
+
+ECO_num = StringVar()
+ecoEntry = ttk.Entry(root, textvariable=ECO_num).grid(row=2, column=1, sticky=tk.W, ipadx=40, padx=(0,15))  
+
+
+pb = ttk.Progressbar(
+    root,
+    orient='horizontal',
+    mode='determinate',
+    length=320
+)
+pb.grid(row=3, column=0, columnspan=2, padx=10, pady=30, sticky=tk.W)
+value_label = tk.Label(root, text="0%")
+value_label.grid(row=3, column=1, padx=20, columnspan=2, sticky=tk.E)
+
+BOM_report_func = partial(BOM_report_creation, username, password, ECO_num)
+
+create_Report_But = ttk.Button(root, text="Create report", command=BOM_report_func).grid(row=4, column=0, ipadx=20, ipady=10)  
+
+exit_but = ttk.Button(root, text="Exit", command=save_nd_quit)
+exit_but.grid(row=4, column=1, ipadx=20, ipady=10)
+
+def show(event=None): # handler
+    BOM_report_creation(username,password,ECO_num)
+
+
+root.bind('<Return>', show)
+
+root.mainloop()
